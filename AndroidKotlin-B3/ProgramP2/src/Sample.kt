65089d806ea75ee74b57810d413b94aded86adf2
azartechnet <azartechnet@gmail.com>
2024-05-29 14:19:55 +0530

    updated

---
+++ AndroidKotlin-B3/ProgramP2/src/Sample.kt
@@ -0,0 +1,24 @@
+import java.util.Scanner
+
+/*fun main(args:Array<String>)
+{
+     var name="welcome";
+	 var pid=1001;
+     print("Welcome to the Application"+pid);	
+}*/
+
+/*fun main(args:Array<String>)
+{
+	var pid:Int=2001;
+	var pname:String="apple";
+	print("Your pid is::$pid Your pname is::$pname");
+}*/
+
+fun main(args:Array<String>)
+{
+	
+	var r1=Scanner(System.`in`);
+	var a1=r1.nextInt();
+	print("Your value is::"+a1);
+
+}
